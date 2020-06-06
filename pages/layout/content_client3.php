307e1efa4f3d144ceabdaeac40ebf3bf4cf948f4
Daniel Jeziorski <danieljeziorski9811@gmail.com>
2020-06-07 00:11:49 +0200

    Layout formsów dla klienta

--- pages/layout/content_client3.php
+++ pages/layout/content_client3.php
@@ -9,19 +9,116 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v2</li>
+              <li class="breadcrumb-item active">Informacje o koncie</li>
             </ol>
           </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+        </div>        
     </div>
     <!-- /.content-header -->
 
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-       
-        
+      <div class="container-fluid"> 
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card card-secondary">
+              <div class="card-header">
+                <h3 class="card-title">Dane adresowe</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form role="form">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Miejscowość</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Miejścowość">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Kod pocztowy</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Kod pocztowy">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Ulica</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Ulica">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Numer budynku</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Numer budynku">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Numer lokalu</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Numer lokalu">
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Zaktualizuj dane adresowe</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card card-secondary">
+              <div class="card-header">
+                <h3 class="card-title">Dane osobowe</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form role="form">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Imię</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Imię">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Nazwisko</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nazwisko">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Adres e-mail</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Adres e-mail">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Numer telefonu</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Numer telefonu">
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Zaktualizuj dane osobowe</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card card-secondary">
+              <div class="card-header">
+                <h3 class="card-title">Hasło</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form role="form">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Hasło</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Hasło">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Powtórz hasło</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Powtórz hasło">
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Zmień hasło</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div><!--/. container-fluid -->
     </section>
     <!-- /.content -->
